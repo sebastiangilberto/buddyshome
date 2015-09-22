4be45ab0f794fe81d3b9830f3fbb744e6ea9e1e4
sgilberto <sebastiananibalgilberto@gmail.com>
2015-09-22 13:31:37 -0300

    cambio el header y footer

--- target/classes/assets/js/javascript.js
+++ target/classes/assets/js/javascript.js
@@ -1,4 +1,25 @@
-  $(document).ready(function() {
+<!--Datatable-->
+
+$(document).ready(function() {
     $("table#table_experiencias").dataTable();      
-  });
+});
   
+<!--Logo Header-->
+
+$(document).ready(function() {
+	$("#logo-header").hover(function(){
+		$(this).effect("bounce", {times:2}, 500 );	
+	});  
+});
+  
+<!--Logo Footer-->
+  
+$(document).ready(function(){
+	$('#logo-footer').draggable(); 
+});
+
+$(document).ready(function(){
+	$('#logo-footer').dblclick(function(){
+		$(this).toggle("explode");
+   }); 
+});
